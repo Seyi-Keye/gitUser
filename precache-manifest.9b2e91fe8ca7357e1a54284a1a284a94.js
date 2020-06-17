f5a3c7955186ee7f086d6e747c9323c4bc98195f
Oluwaseyi Aromokeye <aromokeyes2@gmail.com>
2020-06-17 19:38:52 +0100

    Updates

---
+++ precache-manifest.9b2e91fe8ca7357e1a54284a1a284a94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d89dedcc3d9c5d23e821cb21d9735d",
+    "url": "/gitUser/index.html"
+  },
+  {
+    "revision": "faa10ee853f44437a2f0",
+    "url": "/gitUser/static/css/main.29402b88.chunk.css"
+  },
+  {
+    "revision": "88a55ba41955ea6fa20f",
+    "url": "/gitUser/static/js/2.8ae0d3e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gitUser/static/js/2.8ae0d3e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa10ee853f44437a2f0",
+    "url": "/gitUser/static/js/main.54b63b45.chunk.js"
+  },
+  {
+    "revision": "ae4534b3a2596e11f736",
+    "url": "/gitUser/static/js/runtime-main.98add49b.js"
+  }
+]);
